09b917c71f615577884188ab892ae8041341f745
vallzin <valmilson@gmail.com>
2024-04-30 14:22:54 -0300

    ecluindo o pritntln e adicionando comentários TODO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe scanner
+        
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pelo scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
     }
 }
