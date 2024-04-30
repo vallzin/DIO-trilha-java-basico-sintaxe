8dca4ec12ce60a2d02af29c96a0c1c9ecf7ee2ef
vallzin <valmilson@gmail.com>
2024-04-30 15:01:29 -0300

    scanner de valores digitados via terminal, msg conta criada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -10,18 +10,23 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        double saldo = 237.48;
-
-
         System.out.println("Por favor, digite o número da Agência!");
-        int numero = scanner.nextInt();
+        int agencia = scanner.nextInt();
 
         System.out.println("Por favor, digite o número da Conta!");
-        String agencia = scanner.next();
+        String numero = scanner.next();
 
         System.out.println("Por favor, digite o nome do Cliente!");
         String cliente = scanner.next();
 
+        System.out.println("Digite o valor de depósito: ");
+        double saldo = scanner.nextDouble();
+
         scanner.close();
+
+        System.out.println("Olá "+ cliente +" obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua Agència é "+agencia+" conta "+numero);
+        System.out.println("e seu saldo "+saldo+" já está disponível para saque");
+        System.out.println("Conta criada");
     }
 }
